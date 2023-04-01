215c6048e30ea975065f39ed61386b0f7826ed22
oscar <44992155+140hz@users.noreply.github.com>
2023-03-31 20:06:25 -0600

    not this repo
    error

--- VISUALSUCIOSv1/js/copiarPortapapel.js
+++
@@ -1,8 +0,0 @@
-function copiarPorta(id_elemento) {
-    var aux = document.createElement("input");
-    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
-    document.body.appendChild(aux);
-    aux.select();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
-}
--- VISUALSUCIOSv1/py/auth.py
+++
@@ -1,4 +0,0 @@
-consumer_key        = 'h6zHqiAEUXstLwc8hA8IvNOGm '
-consumer_secret     = 'tLbrYCZ1upuThCucBhYytSDBoipHTvtKuJDJmxYyS5heFSkW1e'
-access_token        = '2422708117-zCPTBDZZ8rfe98sziP3Z8xcCUAdTczlhh9TmpEG'
-access_token_secret = 'GvTKHMEe4dXn6U3FKGcH8BXcoyO89M43Uhq2aiAnTBRJv'
